f135a9c894e5eb06999b5ad10d01e8f269628cb8
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-11-07 13:25:01 +0900

    write data in db

--- lib/screens/create_todo.dart
+++ lib/screens/create_todo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/service/CreateTodoService.dart';
 import 'package:todolist/widgets/textStr.dart';
 
 class CreateToDo extends StatefulWidget {
@@ -16,6 +17,35 @@ class _CreateToDoState extends State<CreateToDo> {
   void getTitle(String str) {
     setState(() {
       todo = str;
+    });
+  }
+
+  void getDetail(String str){
+    setState(() {
+      detail = str;
+    });
+  }
+
+  void createTap(){
+    var createTodoService = CreateTodoService(todo, detail, dateTime!);
+
+    createTodoService.insertTodo();
+    _showDialog();
+  }
+
+  void _showDialog(){
+    showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: const Text('Alert'),
+        content: const Text("등록 완료"),
+        actions: <Widget>[
+          BackButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+          )
+        ],
+      );
     });
   }
 
@@ -39,10 +69,52 @@ class _CreateToDoState extends State<CreateToDo> {
               height: 40,
             ),
             TextStr(
-                fontSizeNum: 24,
+                fontSizeNum: 20,
                 text: '할 일을 입력하세요',
                 getStr: getTitle,
-                maxLine: 1)
+                maxLine: 1,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextStr(
+              fontSizeNum: 18,
+              text: 'Enter your detail here',
+              getStr: getDetail,
+              maxLine: 12,
+            ),
+            ElevatedButton(onPressed:(){
+              showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate:  DateTime.now(),
+                  lastDate: DateTime(2090),
+                ).then((selectedDate) => setState((){
+                  dateTime = selectedDate;
+                }));},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  elevation : 1 ,
+                ),
+                child: const Text(
+                  "기한 선택",
+                  style: TextStyle(
+                    color:Colors.white,
+                  ),
+                ),
+            ),
+            ElevatedButton(
+                onPressed: createTap,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  elevation: 1
+                ),
+                child: const Text(
+                  "Create",
+                  style: TextStyle(
+                    color:Colors.white,
+                  ),
+                ))
           ],
         ),
       ),
