475e09b654af42388000f9b5fc8047990f237f4f
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-12-11 09:30:38 +0900

    update

--- lib/screens/complete_detail.dart
+++ lib/screens/complete_detail.dart
@@ -63,6 +63,7 @@ class _CompleteDetailState extends State<CompleteDetail> {
   void saveReason(DocumentSnapshot? snapshot) async {
     insertReason(snapshot);
     Navigator.of(context).pop();
+    Navigator.of(context).pop();
   }
 
   Future<void> insertReason(DocumentSnapshot? snapshot) async {
